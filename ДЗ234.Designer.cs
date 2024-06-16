0457d383ac4a9fdb5ac043af5fcaacc11700cba4
Vladlubeka <128980992+Vladlubeka@users.noreply.github.com>
2024-06-16 18:08:28 +0300

    Add files via upload

---
+++ ДЗ234.Designer.cs
@@ -0,0 +1,179 @@
+﻿namespace WindowsFormsApp2
+{
+    partial class Form1
+    {
+        private System.ComponentModel.IContainer components = null;
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        private void InitializeComponent()
+        {
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.textBoxFind = new System.Windows.Forms.TextBox();
+            this.textBoxName = new System.Windows.Forms.TextBox();
+            this.textBoxSurname = new System.Windows.Forms.TextBox();
+            this.textBoxAge = new System.Windows.Forms.TextBox();
+            this.textBoxBalance = new System.Windows.Forms.TextBox();
+            this.buttonFind = new System.Windows.Forms.Button();
+            this.buttonDelete = new System.Windows.Forms.Button();
+            this.buttonCreate = new System.Windows.Forms.Button();
+            this.labelName = new System.Windows.Forms.Label();
+            this.labelSurname = new System.Windows.Forms.Label();
+            this.labelAge = new System.Windows.Forms.Label();
+            this.labelBalance = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(12, 38);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(150, 199);
+            this.listBox1.TabIndex = 0;
+            // 
+            // textBoxFind
+            // 
+            this.textBoxFind.Location = new System.Drawing.Point(12, 12);
+            this.textBoxFind.Name = "textBoxFind";
+            this.textBoxFind.Size = new System.Drawing.Size(150, 20);
+            this.textBoxFind.TabIndex = 1;
+            // 
+            // textBoxName
+            // 
+            this.textBoxName.Location = new System.Drawing.Point(230, 38);
+            this.textBoxName.Name = "textBoxName";
+            this.textBoxName.Size = new System.Drawing.Size(100, 20);
+            this.textBoxName.TabIndex = 2;
+            // 
+            // textBoxSurname
+            // 
+            this.textBoxSurname.Location = new System.Drawing.Point(230, 64);
+            this.textBoxSurname.Name = "textBoxSurname";
+            this.textBoxSurname.Size = new System.Drawing.Size(100, 20);
+            this.textBoxSurname.TabIndex = 3;
+            // 
+            // textBoxAge
+            // 
+            this.textBoxAge.Location = new System.Drawing.Point(230, 90);
+            this.textBoxAge.Name = "textBoxAge";
+            this.textBoxAge.Size = new System.Drawing.Size(100, 20);
+            this.textBoxAge.TabIndex = 4;
+            // 
+            // textBoxBalance
+            // 
+            this.textBoxBalance.Location = new System.Drawing.Point(230, 116);
+            this.textBoxBalance.Name = "textBoxBalance";
+            this.textBoxBalance.Size = new System.Drawing.Size(100, 20);
+            this.textBoxBalance.TabIndex = 5;
+            // 
+            // buttonFind
+            // 
+            this.buttonFind.Location = new System.Drawing.Point(12, 243);
+            this.buttonFind.Name = "buttonFind";
+            this.buttonFind.Size = new System.Drawing.Size(75, 23);
+            this.buttonFind.TabIndex = 6;
+            this.buttonFind.Text = "Find";
+            this.buttonFind.UseVisualStyleBackColor = true;
+            this.buttonFind.Click += new System.EventHandler(this.buttonFind_Click);
+            // 
+            // buttonDelete
+            // 
+            this.buttonDelete.Location = new System.Drawing.Point(87, 243);
+            this.buttonDelete.Name = "buttonDelete";
+            this.buttonDelete.Size = new System.Drawing.Size(75, 23);
+            this.buttonDelete.TabIndex = 7;
+            this.buttonDelete.Text = "Delete";
+            this.buttonDelete.UseVisualStyleBackColor = true;
+            this.buttonDelete.Click += new System.EventHandler(this.buttonDelete_Click);
+            // 
+            // buttonCreate
+            // 
+            this.buttonCreate.Location = new System.Drawing.Point(230, 142);
+            this.buttonCreate.Name = "buttonCreate";
+            this.buttonCreate.Size = new System.Drawing.Size(75, 23);
+            this.buttonCreate.TabIndex = 8;
+            this.buttonCreate.Text = "Create";
+            this.buttonCreate.UseVisualStyleBackColor = true;
+            this.buttonCreate.Click += new System.EventHandler(this.buttonCreate_Click);
+            // 
+            // labelName
+            // 
+            this.labelName.AutoSize = true;
+            this.labelName.Location = new System.Drawing.Point(174, 41);
+            this.labelName.Name = "labelName";
+            this.labelName.Size = new System.Drawing.Size(35, 13);
+            this.labelName.TabIndex = 9;
+            this.labelName.Text = "Name";
+            // 
+            // labelSurname
+            // 
+            this.labelSurname.AutoSize = true;
+            this.labelSurname.Location = new System.Drawing.Point(174, 67);
+            this.labelSurname.Name = "labelSurname";
+            this.labelSurname.Size = new System.Drawing.Size(49, 13);
+            this.labelSurname.TabIndex = 10;
+            this.labelSurname.Text = "Surname";
+            // 
+            // labelAge
+            // 
+            this.labelAge.AutoSize = true;
+            this.labelAge.Location = new System.Drawing.Point(174, 93);
+            this.labelAge.Name = "labelAge";
+            this.labelAge.Size = new System.Drawing.Size(26, 13);
+            this.labelAge.TabIndex = 11;
+            this.labelAge.Text = "Age";
+            // 
+            // labelBalance
+            // 
+            this.labelBalance.AutoSize = true;
+            this.labelBalance.Location = new System.Drawing.Point(174, 119);
+            this.labelBalance.Name = "labelBalance";
+            this.labelBalance.Size = new System.Drawing.Size(46, 13);
+            this.labelBalance.TabIndex = 12;
+            this.labelBalance.Text = "Balance";
+            // 
+            // Form1
+            // 
+            this.ClientSize = new System.Drawing.Size(384, 311);
+            this.Controls.Add(this.labelBalance);
+            this.Controls.Add(this.labelAge);
+            this.Controls.Add(this.labelSurname);
+            this.Controls.Add(this.labelName);
+            this.Controls.Add(this.buttonCreate);
+            this.Controls.Add(this.buttonDelete);
+            this.Controls.Add(this.buttonFind);
+            this.Controls.Add(this.textBoxBalance);
+            this.Controls.Add(this.textBoxAge);
+            this.Controls.Add(this.textBoxSurname);
+            this.Controls.Add(this.textBoxName);
+            this.Controls.Add(this.textBoxFind);
+            this.Controls.Add(this.listBox1);
+            this.Name = "Form1";
+            this.Text = "User Management";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+        }
+
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.TextBox textBoxFind;
+        private System.Windows.Forms.TextBox textBoxName;
+        private System.Windows.Forms.TextBox textBoxSurname;
+        private System.Windows.Forms.TextBox textBoxAge;
+        private System.Windows.Forms.TextBox textBoxBalance;
+        private System.Windows.Forms.Button buttonFind;
+        private System.Windows.Forms.Button buttonDelete;
+        private System.Windows.Forms.Button buttonCreate;
+        private System.Windows.Forms.Label labelName;
+        private System.Windows.Forms.Label labelSurname;
+        private System.Windows.Forms.Label labelAge;
+        private System.Windows.Forms.Label labelBalance;
+    }
+}
+
---
+++ ДЗ234.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp2
+{
+    public class User
+    {
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public int Age { get; set; }
+        public decimal Balance { get; set; }
+
+        public User(string name, string surname, int age, decimal balance)
+        {
+            Name = name;
+            Surname = surname;
+            Age = age;
+            Balance = balance;
+        }
+
+        public override string ToString()
+        {
+            return $"{Name} {Surname}, Age: {Age}, Balance: {Balance:C}";
+        }
+    }
+
+    public partial class Form1 : Form
+    {
+        private List<User> users = new List<User>();
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void buttonCreate_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                string name = Name.Text;
+                string surname = Surname.Text;
+                int age = int.Parse(Age.Text);
+                decimal balance = decimal.Parse(Balance.Text);
+
+                User user = new User(name, surname, age, balance);
+                users.Add(user);
+
+                UpdateUserList();
+                ClearInputFields();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error creating user: " + ex.Message);
+            }
+        }
+
+        private void buttonDelete_Click(object sender, EventArgs e)
+        {
+            if (listBox1.SelectedIndex != -1)
+            {
+                users.RemoveAt(listBox1.SelectedIndex);
+                UpdateUserList();
+            }
+        }
+
+        private void buttonFind_Click(object sender, EventArgs e)
+        {
+            string searchQuery = Find.Text.ToLower();
+            var filteredUsers = users.Where(user => user.Name.ToLower().Contains(searchQuery) || user.Surname.ToLower().Contains(searchQuery)).ToList();
+            listBox1.Items.Clear();
+            foreach (var user in filteredUsers)
+            {
+                listBox1.Items.Add(user);
+            }
+        }
+
+        private void UpdateUserList()
+        {
+            listBox1.Items.Clear();
+            foreach (var user in users)
+            {
+                listBox1.Items.Add(user);
+            }
+        }
+
+        private void ClearInputFields()
+        {
+            Name.Clear();
+            Surname.Clear();
+            Age.Clear();
+            Balance.Clear();
+        }
+    }
+}
